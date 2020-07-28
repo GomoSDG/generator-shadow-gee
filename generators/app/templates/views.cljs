4251c15d28616bd780a2162b535c4532d0899bc2
Gomotso <gomotsol@navigare.co.za>
2020-07-28 21:54:17 +0200

    Revert "Revert "initial commit""
    This reverts commit 86381e0463c844b362c60f2c2498deabf7ce559d.

---
+++ generators/app/templates/views.cljs
@@ -0,0 +1,18 @@
+(ns <%= appname %>.views
+    (:require [g5g.panels.home :as home]
+              [re-frame.core :as re-frame]))
+
+(def panels {:home #'home/home})
+
+(re-frame/reg-event-db
+ :set-active-panel
+ (fn [db [_ panel]]
+   (assoc db :active-pane panel)))
+
+(re-frame/reg-sub
+ :active-panel
+ (fn [db]
+   (let [panels          (:panels db)
+         active-panel    (:active-panel db)
+         active-panel-fn (panels active-panel)]
+     active-panel-fn)))
